5e45ebf7f9c54858121e79066ef2298339206acd
Ahmad <ahmadasg2366@gmail.com>
2023-07-16 22:59:38 +0500

    Csv Parser fn completed

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,56 @@
 const express = require('express');
+const multer = require('multer');
 const cors = require('cors');
+const csv = require('csv-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(cors());
 
-app.get('/api', (req, res) => {
-    res.send({
-        message: 'Your backend server is running. Time to create a frontend now!'
-    });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+
+app.post('/upload', upload.single('csvFile'), (req, res) => {
+    const results = {};
+
+    // Parse the uploaded CSV file
+    fs.createReadStream(req.file.path)
+        .pipe(csv())
+        .on('data', (data) => {
+            // Validate the CSV data
+            const modelNumber = data['Model Number'];
+            const unitPrice = parseFloat(data['Unit Price']);
+            const quantity = parseInt(data['Quantity']);
+
+            // check if the modekNumber is a string and unitPrice if float and quantity is int
+            if (typeof modelNumber !== 'string' || isNaN(unitPrice) || isNaN(quantity)) {
+                results['data'] = data;
+                results['message'] = 'Validation failed';
+                results['status'] = 'failed';
+                res.send(results);
+                return;
+            }
+            res.send({ message: 'Validation passed', data: csvModel, status: "success" });
+        })
 });
 
 const port = 4000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+database.on('error', (error) => {
+    console.log(error)
+})
 
+database.once('connected', () => {
+    console.log('Database Connected');
+})
